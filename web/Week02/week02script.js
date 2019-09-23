ca6a46204554e1f36439d50627b9b196b790e8de
Heather Mason <wil06095@byui.edu>
2019-09-23 12:00:19 -0600

    Team Activity week02

---
+++ web/Week02/week02script.js
@@ -0,0 +1,7 @@
+function clicked() {
+    window.alert("Clicked!");
+}
+function changeColor() {
+    var color = document.getElementById("color");
+    document.getElementById("div1").style.backgroundColor = color;
+}
