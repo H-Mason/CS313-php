2bf8c65e6849d4cf831b19d0f0043f2cc310a88f
Heather Mason <wil06095@byui.edu>
2019-10-16 11:05:30 -0600

    setting up connection

---
+++ web/Project01/animalLookup.php
@@ -0,0 +1,14 @@
+<?php
+  require '../../db/dbConnect.php';
+  $db = get_db();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="utf-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Scriptures</title>
+</head>
+<body>
+Something is here
+</body>
