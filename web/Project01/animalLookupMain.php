909a100379807259b7b90cc33fb696e1a3bc83ab
Heather Mason <wil06095@byui.edu>
2019-10-16 11:34:40 -0600

    setting up connection

---
+++ web/Project01/animalLookupMain.php
@@ -0,0 +1,56 @@
+<?php
+  require '../../db/dbConnect.php';
+  $db = get_db();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="project01.css">
+  <title>Mammals of North America</title>
+</head>
+<body>
+  <h1>Learn about Mammals!</h1>
+  <h2>What would you like to do?</h2>
+
+  <ul>
+      <li>
+         <a href="animalLookupName.php">Find Animal by Name</a>
+      </li>
+      <!-- <li>
+         <a>Week 08</a>
+      </li>
+      <li>
+         <a href="https://desolate-earth-41657.herokuapp.com/Week03/Week03Browse.php">Week 03</a>
+      </li>
+      <li>
+         <a>Week 09</a>
+      </li>
+      <li>
+         <a>Week 04</a>
+      </li>
+      <li>
+         <a>Week 10</a>
+      </li>
+      <li>
+         <a>Week 05</a>
+      </li>
+      <li>
+         <a>Week 11</a>
+      </li>
+      <li>
+         <a>Week 06</a>
+      </li>
+      <li>
+         <a>Week 12</a>
+      </li>
+      <li>
+         <a>Week 07</a>
+      </li>
+      <li>
+         <a>Week 13</a>
+      </li> -->
+    </ul>
+  
+</body>
