e0b8f1c0ac7c332168aa666dc9d011dd8888f438
Heather Mason <wil06095@byui.edu>
2019-10-17 16:05:58 -0600

    UI project 1

--- web/Project01/animalLookupName.php
+++ web/Project01/animalLookupName.php
@@ -19,22 +19,22 @@
     <?php
       
       $stmt = ($db->query("SELECT animals.animal_name,
-                  animals.picture,
-                  animals.description,
-                  genus.genus AS 'genus',
-                  family.family AS 'family',
-                  a_order.order_name AS 'order',
-                  size.size AS 'size',
-                  animals.size_description,
-                  animals.region,
-                  diet.diet AS 'diet'
-            FROM   animals
-            JOIN   genus ON genus.genus_id = animals.genus_id
-            JOIN   family ON family.family_id = animals.family_id 
-            JOIN   a_order ON a_order.order_id = animals.order_id
-            JOIN   size ON size.size_id = animals.size_id
-            JOIN   diet ON diet.diet_id = animals.diet_id
-            where  animals.animal_name = 'Mule Deer';");
+      animals.picture,
+      animals.description,
+      genus.genus AS genus,
+      family.family AS family,
+      a_order.order_name AS a_order,
+      size.size AS size,
+      animals.size_description,
+      animals.region,
+      diet.diet AS diet
+FROM   animals
+JOIN   genus ON genus.genus_id = animals.genus_id
+JOIN   family ON family.family_id = animals.family_id 
+JOIN   a_order ON a_order.order_id = animals.order_id
+JOIN   size ON size.size_id = animals.size_id
+JOIN   diet ON diet.diet_id = animals.diet_id
+where  animals.animal_name = 'Mule Deer'");
       
       print('Animal name: ' . $stmt['animal_name'] . '<br>');
       print('Species: ' . $stmt['scientific_name'] . '<br>');
