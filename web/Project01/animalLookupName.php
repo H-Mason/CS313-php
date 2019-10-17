f8d3e7b1c27977cb5917b0de499dfd62742d5d48
Heather Mason <wil06095@byui.edu>
2019-10-17 16:10:59 -0600

    UI project 1

--- web/Project01/animalLookupName.php
+++ web/Project01/animalLookupName.php
@@ -33,7 +33,9 @@ JOIN   genus ON genus.genus_id = animals.genus_id
 JOIN   family ON family.family_id = animals.family_id 
 JOIN   a_order ON a_order.order_id = animals.order_id
 JOIN   size ON size.size_id = animals.size_id
-JOIN   diet ON diet.diet_id = animals.diet_id");
+JOIN   diet ON diet.diet_id = animals.diet_id
+where  animals.animal_name = 'Mule Deer'");
+$stmt->execute();
       
       print('Animal name: ' . $stmt['animal_name'] . '<br>');
       print('Species: ' . $stmt['scientific_name'] . '<br>');
