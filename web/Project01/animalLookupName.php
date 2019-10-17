5cc16d93390f8c2d1bb00f7c7447f40450183338
Heather Mason <wil06095@byui.edu>
2019-10-17 14:38:41 -0600

    UI project 1

--- web/Project01/animalLookupName.php
+++ web/Project01/animalLookupName.php
@@ -1,6 +1,11 @@
 <?php
   require '../../db/dbConnect.php';
   $db = get_db();
+  $stmt = $db->prepare("SELECT *
+                        from animals
+                        LEFT JOIN genus ON genus.genus_id=animals.genus.id
+                        ")
+  $stmt->execute();
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -14,24 +19,25 @@
   <h1>Learn about Mammals!</h1>
   <div>
     <?php
-      foreach ($db->query("SELECT animal_name, picture, description, scientific_name, 
-      genus_id, family_id, order_id, size_id, size_description, region, diet_id 
-      FROM animals") as $row) {
-        print('Animal name: ' . $row['animal_name'] . '<br>');
-        print('Species: ' . $row['scientific_name'] . '<br>');
-        // $genus_id = $row['genus_id'];
-        // $genus = ($db->query("SELECT genus FROM genus WHERE genus_id=$genus_id"));
-        // print('Genus: ' . $genus);
-        print('<img src=\'../project1Data/' . $row['picture'] . '\'<br>');
-        $descFile = '../project1Data/' . $row['description'];
-        $desc = fopen($descFile, "r") or die("Unable to open file!");
-        print('<div class=\'desc\'>' . fread($desc,filesize($descFile)) . '</div><br>');
-        fclose($desc);
-        $descFile = '../project1Data/' . $row['size_description'];
-        $desc = fopen($descFile, "r") or die("Unable to open file!");
-        print('<div class=\'desc\'>' . fread($desc,filesize($descFile)) . '</div><br>');
-        fclose($desc);
-        print('<img src=\'../project1Data/' . $row['region'] . '\'<br>');
+      print("genus: " . $stmt.genus);
+      // foreach ($db->query("SELECT animal_name, picture, description, scientific_name, 
+      // genus_id, family_id, order_id, size_id, size_description, region, diet_id 
+      // FROM animals") as $row) {
+      //   print('Animal name: ' . $row['animal_name'] . '<br>');
+      //   print('Species: ' . $row['scientific_name'] . '<br>');
+      //   // $genus_id = $row['genus_id'];
+      //   // $genus = ($db->query("SELECT genus FROM genus WHERE genus_id=$genus_id"));
+      //   // print('Genus: ' . $genus);
+      //   print('<img src=\'../project1Data/' . $row['picture'] . '\'<br>');
+      //   $descFile = '../project1Data/' . $row['description'];
+      //   $desc = fopen($descFile, "r") or die("Unable to open file!");
+      //   print('<div class=\'desc\'>' . fread($desc,filesize($descFile)) . '</div><br>');
+      //   fclose($desc);
+      //   $descFile = '../project1Data/' . $row['size_description'];
+      //   $desc = fopen($descFile, "r") or die("Unable to open file!");
+      //   print('<div class=\'desc\'>' . fread($desc,filesize($descFile)) . '</div><br>');
+      //   fclose($desc);
+      //   print('<img src=\'../project1Data/' . $row['region'] . '\'<br>');
       }
     ?>
   </div>
