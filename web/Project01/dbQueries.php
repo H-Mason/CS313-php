44c7b8a5b0d6b44c48a97b4b312b51a1aea904d2
Heather Mason <wil06095@byui.edu>
2019-10-17 15:27:30 -0600

    UI project 1

--- web/Project01/dbQueries.php
+++ web/Project01/dbQueries.php
@@ -1,9 +1,12 @@
 <?php
 require '../../db/dbConnect.php';
 $db = get_db();
-function getOneAnimal($animalName) {
+function getGenus($animalName) {
     $stmt = $db->prepare("SELECT *
-    ");
+    from animals
+    INNER JOIN genus 
+    ON genus.genus=animals.genus_id
+    WHERE genus.genus_id=1");
     $stmt->execute();
     
 }
