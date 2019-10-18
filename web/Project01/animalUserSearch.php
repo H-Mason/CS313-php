4c19af86d971f6923f476367b28b670de21e7e9f
Heather Mason <wil06095@byui.edu>
2019-10-17 23:18:17 -0600

    project 1 database read-only

--- web/Project01/animalUserSearch.php
+++ web/Project01/animalUserSearch.php
@@ -15,47 +15,47 @@
   <div>
     <?php
         if (isset($_POST['byName'])) {
-            print("<form action='animalLookup.php' method='post' id='inputForm' name='byName'>");
+            print("<form action='animalLookup.php' method='post' id='inputForm' >");
             print("Animal Name: <input type='text' class='input' name='input'><br><br>");
-            print("<input type='submit' class='directory' id='submit' value='Search'>");
+            print("<input type='submit' class='directory' id='submit' name='byName' value='Search'>");
             print("</form>");
         }
         if (isset($_POST['bySize'])) {
-            print("<form action='animalLookup.php' method='post' id='inputForm' name='bySize'>");
+            print("<form action='animalLookup.php' method='post' id='inputForm'>");
             print("Size: <select class='input' name='input'>");
             print("<option value='Small'>Small</option>");
             print("<option value='Medium'>Medium</option>");
             print("<option value='Large'>Large</option>");
             print("</select><br><br>");
-            print("<input type='submit' class='directory' id='submit' value='Search'>");
+            print("<input type='submit' class='directory' id='submit' name='bySize' value='Search'>");
             print("</form>");
         }
         if (isset($_POST['byGenus'])) {
-            print("<form action='animalLookup.php' method='post' id='inputForm' name='byGenus'>");
+            print("<form action='animalLookup.php' method='post' id='inputForm'>");
             print("Genus: <input type='text' class='input' name='input'><br><br>");
-            print("<input type='submit' class='directory' id='submit' value='Search'>");
+            print("<input type='submit' class='directory' id='submit' name='byGenus' value='Search'>");
             print("</form>");
         }
         if (isset($_POST['byFamily'])) {
-            print("<form action='animalLookup.php' method='post' id='inputForm' name='byFamily'>");
+            print("<form action='animalLookup.php' method='post' id='inputForm'>");
             print("Family: <input type='text' class='input' name='input'><br><br>");
-            print("<input type='submit' class='directory' id='submit' value='Search'>");
+            print("<input type='submit' class='directory' id='submit' name='byFamily' value='Search'>");
             print("</form>");
         }
         if (isset($_POST['byOrder'])) {
-            print("<form action='animalLookup.php' method='post' id='inputForm' name='byOrder'>");
+            print("<form action='animalLookup.php' method='post' id='inputForm'>");
             print("Order: <input type='text' class='input' name='input'><br><br>");
-            print("<input type='submit' class='directory' id='submit' value='Search'>");
+            print("<input type='submit' class='directory' id='submit' name='byOrder' value='Search'>");
             print("</form>");
         }
         if (isset($_POST['byDiet'])) {
-            print("<form action='animalLookup.php' method='post' id='inputForm' name='byDiet'>");
+            print("<form action='animalLookup.php' method='post' id='inputForm'>");
             print("Diet: <select class='input' name='input'>");
             print("<option value='Herbivore'>Herbivore</option>");
             print("<option value='Omnivore'>Omnivore</option>");
             print("<option value='Carnivore'>Carnivore</option>");
             print("</select><br><br>");
-            print("<input type='submit' class='directory' id='submit' value='Search'>");
+            print("<input type='submit' class='directory' id='submit' name='byDiet' value='Search'>");
             print("</form>");
         }
     ?>
