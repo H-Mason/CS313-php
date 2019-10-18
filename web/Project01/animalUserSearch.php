05b6c686e31e89c674bf992559b9d64d91158c35
Heather Mason <wil06095@byui.edu>
2019-10-18 00:27:45 -0600

    project 1 database read-only

--- web/Project01/animalUserSearch.php
+++ web/Project01/animalUserSearch.php
@@ -58,7 +58,8 @@
             print("<input type='submit' class='directory' id='submit' name='byDiet' value='Search'>");
             print("</form>");
         }
-        print("<a href='animalLookupMain.php'><ul class='directory'>Return Home</ul></a>");
+        print("</div>");
+        print("<a href='animalLookupMain.php'><ul class='directory'>Return Home</ul></a><div>");
     ?>
   </div>
 </body>
