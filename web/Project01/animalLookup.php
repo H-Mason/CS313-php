74e93c2adb299fe234be24a99dab86f9cdebf6bb
Heather Mason <wil06095@byui.edu>
2019-10-17 23:03:39 -0600

    project 1 database read-only

--- web/Project01/animalLookup.php
+++ web/Project01/animalLookup.php
@@ -15,40 +15,63 @@
   <div>
     <?php
         if (isset($_POST['byName'])) {
-            print("<form action='animalLookup.php' method='post' id='inputForm' name='inputForm'>");
-            print("Animal Name: <input type='text' class='input' name='input'><br><br>");
-            print("<input type='submit' class='directory' id='submit' value='Search'>");
-            print("</form>");
+            foreach ($db->query("SELECT animals.animal_name,
+                                  animals.picture,
+                                  animals.description,
+                                  animals.scientific_name,
+                                  genus.genus AS genus,
+                                  family.family AS family,
+                                  a_order.order_name AS a_order,
+                                  size.size AS size,
+                                  animals.size_description,
+                                  animals.region,
+                                  diet.diet AS diet
+                            FROM   animals
+                            JOIN   genus ON genus.genus_id = animals.genus_id
+                            JOIN   family ON family.family_id = animals.family_id 
+                            JOIN   a_order ON a_order.order_id = animals.order_id
+                            JOIN   size ON size.size_id = animals.size_id
+                            JOIN   diet ON diet.diet_id = animals.diet_id
+                            WHERE  animals.animal_name = 'Mule Deer'") as $row) 
+            {
+                if ($row != NULL) {
+                    print('Animal name: ' . $row['animal_name'] . '<br>');
+                    print('Species: ' . $row['scientific_name'] . '<br>');
+                    print('Genus: ' . $row['genus'] . '<br>');
+                    print('Family: ' . $row['family'] . '<br>');
+                    print('Order: ' . $row['a_order'] . '<br>');
+                    print('Diet: ' . $row['diet'] . '<br>');
+                    print('Size: ' . $row['size'] . '<br>');
+                    print('<img src=\'../project1Data/' . $row['picture'] . '\'<br>');
+                    $descFile = '../project1Data/' . $row['description'];
+                    $desc = fopen($descFile, "r") or die("Unable to open file!");
+                    print('<div class=\'desc\'>' . fread($desc,filesize($descFile)) . '</div><br>');
+                    fclose($desc);
+                    $descFile = '../project1Data/' . $row['size_description'];
+                    $desc = fopen($descFile, "r") or die("Unable to open file!");
+                    print('<div class=\'desc\'>' . fread($desc,filesize($descFile)) . '</div><br>');
+                    fclose($desc);
+                    print('<img src=\'../project1Data/' . $row['region'] . '\'<br>'); 
+                }
+                else {
+                    print("No animals found")
+                }
+            }
         }
         if (isset($_POST['bySize'])) {
-            print("<form action='animalLookup.php' method='post' id='inputForm' name='inputForm'>");
-            print("Animal Name: <input type='text' class='input' name='input'><br><br>");
-            print("<input type='submit' class='directory' id='submit' value='Search'>");
-            print("</form>");
+            
         }
         if (isset($_POST['byGenus'])) {
-            print("<form action='animalLookup.php' method='post' id='inputForm' name='inputForm'>");
-            print("Animal Name: <input type='text' class='input' name='input'><br><br>");
-            print("<input type='submit' class='directory' id='submit' value='Search'>");
-            print("</form>");
+            
         }
         if (isset($_POST['byFamily'])) {
-            print("<form action='animalLookup.php' method='post' id='inputForm' name='inputForm'>");
-            print("Animal Name: <input type='text' class='input' name='input'><br><br>");
-            print("<input type='submit' class='directory' id='submit' value='Search'>");
-            print("</form>");
+            
         }
         if (isset($_POST['byOrder'])) {
-            print("<form action='animalLookup.php' method='post' id='inputForm' name='inputForm'>");
-            print("Animal Name: <input type='text' class='input' name='input'><br><br>");
-            print("<input type='submit' class='directory' id='submit' value='Search'>");
-            print("</form>");
+            
         }
         if (isset($_POST['byDiet'])) {
-            print("<form action='animalLookup.php' method='post' id='inputForm' name='inputForm'>");
-            print("Animal Name: <input type='text' class='input' name='input'><br><br>");
-            print("<input type='submit' class='directory' id='submit' value='Search'>");
-            print("</form>");
+            
         }
     ?>
   </div>
