0686c29f367b638a543637f7af3df98585aa44ad
Heather Mason <wil06095@byui.edu>
2019-10-22 15:48:25 -0600

    team activity

--- web/Week06/teamActivity06_2.php
+++ web/Week06/teamActivity06_2.php
@@ -10,8 +10,8 @@
   $stmt = $db->prepare('INSERT INTO scripture (book, chapter, verse, content) 
                         VALUES (:book, :chapter, :verse, :content);');
   $stmt->bindValue(':book', $book, PDO::PARAM_STR);
-  $stmt->bindValue(':chapter', $chapter, PDO::PARAM_STR);
-  $stmt->bindValue(':verse', $verse, PDO::PARAM_STR);
+  $stmt->bindValue(':chapter', $chapter, PDO::PARAM_INT);
+  $stmt->bindValue(':verse', $verse, PDO::PARAM_INT);
   $stmt->bindValue(':content', $content, PDO::PARAM_STR);
   $stmt->execute();
 
