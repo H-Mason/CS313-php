80d495dea1b66b239ff367c3996b7f2ef07edf24
Heather Mason <wil06095@byui.edu>
2019-10-29 15:20:34 -0600

    adding picture upload

--- web/Project01/addAnimalConf.php
+++ web/Project01/addAnimalConf.php
@@ -36,32 +36,32 @@
             }
         }
         // Check if file already exists
-        if (file_exists($target_file)) {
-            echo "Sorry, a picture with this name already exists.";
-            $uploadOk = 0;
-        }
-        // Check file size
-        if ($_FILES[$picName]["size"] > 500000) {
-            echo "Sorry, your file is too large.";
-            $uploadOk = 0;
-        }
+        // if (file_exists($target_file)) {
+        //     echo "Sorry, a picture with this name already exists.";
+        //     $uploadOk = 0;
+        // }
+        // // Check file size
+        // if ($_FILES[$picName]["size"] > 500000) {
+        //     echo "Sorry, your file is too large.";
+        //     $uploadOk = 0;
+        // }
         // // Allow certain file formats
         // if($imageFileType != "jpg" && $imageFileType != "png" 
         // && $imageFileType != "jpeg") {
         //     echo "Sorry, only JPG, JPEG, & PNG files are allowed.";
         //     $uploadOk = 0;
         // }
-        // Check if $uploadOk is set to 0 by an error
-        if ($uploadOk == 0) {
-            echo "Sorry, your file was not uploaded.";
-        // if everything is ok, try to upload file
-        } else {
-            if (move_uploaded_file($_FILES[$picName]["tmp_name"], $target_file)) {
-                echo "The file ". basename( $_FILES[$picName]["name"]). " has been uploaded.";
-            } else {
-                echo "Sorry, there was an error uploading your file.";
-            }
-        }
+        // // Check if $uploadOk is set to 0 by an error
+        // if ($uploadOk == 0) {
+        //     echo "Sorry, your file was not uploaded.";
+        // // if everything is ok, try to upload file
+        // } else {
+        //     if (move_uploaded_file($_FILES[$picName]["tmp_name"], $target_file)) {
+        //         echo "The file ". basename( $_FILES[$picName]["name"]). " has been uploaded.";
+        //     } else {
+        //         echo "Sorry, there was an error uploading your file.";
+        //     }
+        // }
         return $target_file;
     }
 
