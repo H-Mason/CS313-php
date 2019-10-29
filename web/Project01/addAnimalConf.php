625e1f78118d7f506a45134042c75cc0d6342277
Heather Mason <wil06095@byui.edu>
2019-10-29 17:28:25 -0600

    adding picture upload

--- web/Project01/addAnimalConf.php
+++ web/Project01/addAnimalConf.php
@@ -22,6 +22,7 @@
 
     function savePic($filepath, $picName) {
         $target_file = $filepath . basename($_FILES[$picName]["name"]);
+        echo $target_file;
         $uploadOk = 1;
         $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
         // Check if image file is a actual image or fake image
